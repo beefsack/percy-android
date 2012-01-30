f1f5bb81db49c5ae24e0f4bd57d1108540c1020b
Michael Alexander <beefsack@gmail.com>
2012-01-30 22:12:14 +0800

    Initialising git repos.

---
+++ src/com/beefsack/percy/Account.java
@@ -0,0 +1,10 @@
+package com.beefsack.percy;
+
+public class Account {
+	
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/com/beefsack/percy/PercyActivity.java
+++ src/com/beefsack/percy/PercyActivity.java
@@ -1,13 +1,31 @@
 package com.beefsack.percy;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class PercyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		File repoDir = getDir("repo", MODE_PRIVATE);
+		try {
+			Git git;
+			try {
+				git = Git.open(repoDir);
+			} catch (RepositoryNotFoundException e) {
+				git = Git.init().setDirectory(repoDir).call();
+			}
+			System.out.println(git.status().call().toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
